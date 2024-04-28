d826562640d038c420cecae6c171b07ac3d7045d
anapedra <anapedra.mil@gmail.com>
2024-04-27 21:10:26 -0300

    Project DesafioControleFluxo created

---
+++ DesafioControleFluxo/src/main/java/com/anapedra/desafiocontrolefluxo/Contador.java
@@ -0,0 +1,13 @@
+package com.anapedra.desafiocontrolefluxo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Contador {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Contador.class, args);
+    }
+
+}
---
+++ DesafioControleFluxo/src/test/java/com/anapedra/desafiocontrolefluxo/DesafioControleFluxoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anapedra.desafiocontrolefluxo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioControleFluxoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
